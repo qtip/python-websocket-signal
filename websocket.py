13feb4fe93b28ea263283d021016e7851867565a
qtip <qtipkk@gmail.com>
2010-11-10 15:21:03 -0600

    Update the server action a little. Tried sending stuff back to the client. Still no luck on shutting it down properly

--- websocket.py
+++ websocket.py
@@ -3,8 +3,20 @@ from SocketServer import TCPServer, ThreadingMixIn, BaseRequestHandler
 from sys import argv
 from string import count, rjust
 from hashlib import md5
-class ThreadingTCPServer(ThreadingMixIn, TCPServer): pass
 
+class ThreadingTCPServer(ThreadingMixIn, TCPServer):
+    def __init__(self, *args, **kwargs):
+        TCPServer.__init__(self, *args, **kwargs)
+        self.connections = dict()
+        
+    def get_request(self):
+        request, client_address = TCPServer.get_request(self)
+        self.connections[client_address] = request
+        return request, client_address
+    def send_to_all(self, msg):
+        for client_address, connection in self.connections.iteritems():
+            connection.send(msg)
+    
 def number_to_bytes(n):
     out = ''
     while n > 0:
@@ -17,6 +29,9 @@ def sec_websocket_key_decode(s):
     number = int(''.join([x for x in s if x in '0123456789']))
     spaces = count(s, ' ')
     return number_to_bytes(int(number/spaces))
+
+def websocket_msg(s):
+    return '\x00' + s + '\xff'
 
 class WebSocketTCPHandler(BaseRequestHandler):
     """
@@ -33,7 +48,6 @@ class WebSocketTCPHandler(BaseRequestHandler):
         if fields['Connection'] == 'Upgrade' and fields['Upgrade'] == 'WebSocket':
             number1 = sec_websocket_key_decode(fields['Sec-WebSocket-Key1'])
             number2 = sec_websocket_key_decode(fields['Sec-WebSocket-Key2'])
-            print md5(number1 + number2 + data).digest()
             response = []
             response.append("HTTP/1.1 101 WebSocket Protocol Handshake")
             response.append("Upgrade: WebSocket")
@@ -47,7 +61,14 @@ class WebSocketTCPHandler(BaseRequestHandler):
                 request = self.request.recv(1024)
                 if not request:
                     break
-                print repr(request[1:-1])
+                msg = request[1:-1]
+                print repr(msg)
+                print repr(request[0])
+                print repr(request[-1])
+                if msg == 'hi':
+                    self.server.send_to_all(websocket_msg('why hello there'))
+                elif msg == 'exit':
+                    self.server.shutdown()
         else:
             # not a websocket request.
             print 'failure'
@@ -60,5 +81,5 @@ if __name__ == "__main__":
     server = ThreadingTCPServer(('127.0.0.1', port), WebSocketTCPHandler)
     try:
         server.serve_forever()
-    except KeyboardInterrupt:
-        pass
+    except (KeyboardInterrupt):
+        exit()
